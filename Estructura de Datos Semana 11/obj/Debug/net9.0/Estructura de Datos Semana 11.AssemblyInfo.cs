b783fd6d781f5905ba62e841c784ed087b8c214e
JOSE JHAIR HERNANDEZ TSEREMP <jj.hernandezt@uea.edu.ec>
2025-03-01 20:04:36 -0500

    Semana 11

--- Estructura de Datos Semana 11/obj/Debug/net9.0/Estructura de Datos Semana 11.AssemblyInfo.cs
+++ Estructura de Datos Semana 11/obj/Debug/net9.0/Estructura de Datos Semana 11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructura de Datos Semana 11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea06fcc0cf26bf262f1f39b19e50f028b2744976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae191577a38c98b157cdabbae0e272e23577d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructura de Datos Semana 11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructura de Datos Semana 11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
